5cf56ed8805707135474648190cf49c494354c07
Cordero <chernandez@rapidbizApps.com>
2017-09-29 13:32:51 -0700

    Adds IBOutlets

---
+++ mlScanner/Controllers/CameraVC.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  mlScanner
+//
+//  Created by Cordero Hernandez on 9/29/17.
+//  Copyright © 2017 Cordero Hernandez. All rights reserved.
+//
+
+import AVFoundation
+import UIKit
+
+class CameraVC: UIViewController {
+    
+    @IBOutlet var identificationLabel: UILabel!
+    @IBOutlet var confidentLabel: UILabel!
+    @IBOutlet var flashButton: UIButton!
+    @IBOutlet var cameraView: UIView!
+    @IBOutlet var thumbnailCameraImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+
+}
+
